a1ea00968d83d08c98c575c5d187f2ca4fd68920
Michael Waide <mvwaide@gmail.com>
2020-06-30 18:20:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
 
 
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
